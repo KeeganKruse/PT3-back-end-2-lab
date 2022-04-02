f7dfecef7b5b15c68dfbd4a27eb54d3728537974
Keegan Kruse <krusekeegan@gmail.com>
2022-04-02 16:15:13 -0600

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const PORT = 4004
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const {
+    getHouses,
+    deleteHouse,
+    createHouse,
+    updateHouse
+} = require('./controller')
+
+app.get(`/api/houses`, getHouses)
+app.delete(`api/houses/id:`, deleteHouse)
+app.post(`api/houses/`, createHouse)
+app.put(`api/houses/id:`, updateHouse)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
